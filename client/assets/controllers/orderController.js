895b4736edf313ddfa8e2195ba6ef45dc5192d0f
Priyanka <priyanka3007@gmail.com>
2016-08-31 17:11:26 -0400

    order status

--- client/assets/controllers/orderController.js
+++ client/assets/controllers/orderController.js
@@ -1,3 +1,21 @@
 app.controller('orderController', ['$scope', '$location', 'userFactory', '$cookies', function($scope, $location, userFactory, $cookies) {
 
+$scope.getCustomer = function() {
+        userFactory.getCustomer(function(data) {
+            $scope.customers = data
+        })
+    };
+    $scope.getCustomer();
+
+
+$scope.getProduct = function() {
+        userFactory.getProduct(function(data) {
+            $scope.products = data
+        })
+    };
+    $scope.getProduct();
+
+
+
+
 }])
