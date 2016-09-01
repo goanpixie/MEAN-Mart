62dd1ad830105219a16014213e6ae54700fb1c25
Priyanka <priyanka3007@gmail.com>
2016-09-01 08:56:39 -0400

    setup the timeline

--- client/assets/controllers/homeController.js
+++ client/assets/controllers/homeController.js
@@ -1,3 +1,36 @@
 app.controller('homeController', ['$scope', '$location', 'userFactory', '$cookies',function($scope, $location, userFactory, $cookies) {
 
+
+	$scope.newOrder = {};
+    $scope.errors = false;
+    $scope.messages = [];
+    $scope.orders = {};
+    $scope.products =[];
+    $scope.customers = [];
+
+
+
+    $scope.getCustomer = function() {
+        userFactory.getCustomer(function(data) {
+            $scope.customers = data
+        })
+    };
+    $scope.getCustomer();
+
+
+    $scope.getProduct = function() {
+        userFactory.getProduct(function(data) {
+            $scope.products = data
+        })
+    };
+    $scope.getProduct();
+
+
+    $scope.getOrder = function() {
+        userFactory.getOrder(function(data) {
+            $scope.orders = data
+        })
+    };
+    $scope.getOrder();
+
 }])
